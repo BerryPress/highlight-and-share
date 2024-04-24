def6a9c8bb05369b242445c7569e3bb3bd58cd7a
Ronald Huereca <ronald@mediaron.com>
2024-04-24 08:52:58 -0500

    Doing prod build.

--- dist/has-admin-settings.asset.php
+++ dist/has-admin-settings.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-a11y', 'wp-components', 'wp-escape-html', 'wp-i18n'), 'version' => 'd52ee51ffdef505aa57d');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-a11y', 'wp-components', 'wp-escape-html', 'wp-i18n'), 'version' => 'b2265c9cf0b88ddb148d');
