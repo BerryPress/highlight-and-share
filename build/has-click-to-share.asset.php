24e2d26da884474f89ba00be91e4d29615acd253
Ronald Huereca <ronald@mediaron.com>
2025-12-16 21:02:13 -0600

    Ensuring border and dimensions don't error out.

--- build/has-click-to-share.asset.php
+++ build/has-click-to-share.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-a11y', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'e295fc5dc3e5a4502573');
+<?php return array('dependencies' => array('react', 'wp-a11y', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '06415faf5a67284e4f97');
