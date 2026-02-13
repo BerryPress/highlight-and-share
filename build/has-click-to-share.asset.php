4e4ec638026d621870bfcc781390ec218746268b
Ronald Huereca <ronald@mediaron.com>
2026-02-13 02:38:03 -0600

    Readd centering.

--- build/has-click-to-share.asset.php
+++ build/has-click-to-share.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '22963154e8cb96017056');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'e8d7a265ca830f9d757d');
