7d98d291466e378b8ea271dccfe9b6a85eafaf85
Ronald Huereca <ronald@mediaron.com>
2026-02-15 02:50:55 -0600

    Adding readme docs and version bump.

--- build/has-click-to-share.asset.php
+++ build/has-click-to-share.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '01bf3bcc235c457bdad2');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => 'ce146ac7909332825fc9');
