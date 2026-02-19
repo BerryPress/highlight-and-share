e8502e97bcd239246572f5d2f9010a7a1ac1833f
Ronald Huereca <ronald@mediaron.com>
2026-02-19 01:24:19 -0600

    Version bump.

--- build/has-click-to-share.asset.php
+++ build/has-click-to-share.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => 'f3b363dfb5217622b06a');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => 'ce146ac7909332825fc9');
