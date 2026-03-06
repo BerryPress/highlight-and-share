2edb9498e2036c1b8c01aad4f31ecd99662340a1
Ronald Huereca <ronald@mediaron.com>
2026-03-06 05:49:53 -0600

    Version bump.

--- highlight-and-share.php
+++ highlight-and-share.php
@@ -5,7 +5,7 @@
  * Plugin URI: https://dlxplugins.com/plugins/highlight-and-share/
  * Description: Select text, inline highlight, or use a Click to Share block and show social networks.
  * Author: DLX Plugins
- * Version: 6.0.0-rc1
+ * Version: 6.0.0-rc2
  * Requires at least: 6.5
  * Requires PHP: 7.2
  * Author URI: https://dlxplugins.com/plugins/highlight-and-share/
@@ -21,7 +21,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'HIGHLIGHT_AND_SHARE_VERSION', '6.0.0-rc1' );
+define( 'HIGHLIGHT_AND_SHARE_VERSION', '6.0.0-rc2' );
 define( 'HIGHLIGHT_AND_SHARE_OPTIONS_VERSION', '1.0.2' );
 define( 'HIGHLIGHT_AND_SHARE_FILE', __FILE__ );
 
