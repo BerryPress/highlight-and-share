46cf58f482cb8f1f813e469d90a52eb726e3abd1
Ronald Huereca <ronald@mediaron.com>
2026-03-09 20:47:58 -0500

    Fixing tooltip positioning for social networks.

--- dist/has-admin-sharing.asset.php
+++ dist/has-admin-sharing.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-a11y', 'wp-components', 'wp-data', 'wp-escape-html', 'wp-i18n'), 'version' => '8be5249b834c59f1d0a9');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-a11y', 'wp-components', 'wp-data', 'wp-escape-html', 'wp-i18n'), 'version' => '5381bcd624945f03c0ee');
