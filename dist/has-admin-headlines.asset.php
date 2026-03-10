665f2aabcfd324061e2d9f970f95015a65bea01e
Ronald Huereca <ronald@mediaron.com>
2026-03-09 23:17:50 -0500

    Adding headline truncate option.

--- dist/has-admin-headlines.asset.php
+++ dist/has-admin-headlines.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-a11y', 'wp-components', 'wp-data', 'wp-i18n', 'wp-url'), 'version' => 'fdba000f6983bd99b50a');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-a11y', 'wp-components', 'wp-data', 'wp-i18n', 'wp-url'), 'version' => '91d4efe5cd9039904c46');
