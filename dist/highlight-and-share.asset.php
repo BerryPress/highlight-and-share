03867ec5f7d96ecfb6956aa7dbce31a0dcc4aba7
Jonathan Hall <jonathan@berrypress.com>
2026-08-29 06:48:26 -0700

    Remove unused packages, rebuild (no license changes because 'the set of packages actually compiled into dist//build/ is unchanged — all of the above (including react-router) were dead weight that never shipped'

--- dist/highlight-and-share.asset.php
+++ dist/highlight-and-share.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-i18n'), 'version' => 'a2f9bc24229464f017b9');
+<?php return array('dependencies' => array('wp-i18n'), 'version' => 'c0cc5a133f3ff098965b');
