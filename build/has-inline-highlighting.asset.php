3a946bc643e509da8fd9f737f9d15912f9ddba51
Jonathan Hall <jonathan@berrypress.com>
2026-08-29 20:44:26 -0700

    Credit for email validation regex

--- build/has-inline-highlighting.asset.php
+++ build/has-inline-highlighting.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-i18n', 'wp-rich-text'), 'version' => '999bbef52724c6a16bae');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-i18n', 'wp-rich-text'), 'version' => '5356824dc40c97efdcbd');
